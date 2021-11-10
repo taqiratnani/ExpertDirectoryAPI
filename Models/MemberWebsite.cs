4ab44018fc7e8bb3757ce886d9002d2d0b8784ee
Muhammad Taqi <56026493+taqiratnani@users.noreply.github.com>
2021-11-10 18:22:42 +0500

    Delete MemberWebsite.cs

--- Models/MemberWebsite.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ExpertDirectory.Models
-{
-    public class MemberWebsite
-    {
-        public int Id {  get; set; }
-        public int MemberId { get; set; }
-        public string WebAddress { get; set; }
-    }
-}
